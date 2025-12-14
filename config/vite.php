7a0cfbb018915a0329fe8923585e9755d4204b41
Andre Nguyen <a@ackstudio.com>
2025-12-14 17:33:11 +0800

    Squashed commit of the following:
    commit 47ae906cdc3d25165d3dd7f746cd4cf491e7963b
    Author: Andre Nguyen <a@ackstudio.com>
    Date:   Sun Dec 14 17:30:17 2025 +0800
    
        Added: CriticalCSS
    
    commit 63cac2ef47c92dfff5af08eb77baad963a2a17a1
    Author: Andre Nguyen <a@ackstudio.com>
    Date:   Sun Dec 14 09:02:54 2025 +0800
    
        Added - Vite
    
    commit cd488bce78d4209366046893fae9286f4bae6233
    Author: Andre Nguyen <a@ackstudio.com>
    Date:   Sat Dec 13 20:49:09 2025 +0800
    
        Added - Readme

---
+++ config/vite.php
@@ -0,0 +1,18 @@
+<?php
+
+use craft\helpers\App;
+
+return [
+    'useDevServer' => App::env('ENVIRONMENT') === 'dev' || App::env('CRAFT_ENVIRONMENT') === 'dev',
+    'manifestPath' => '@webroot/dist/.vite/manifest.json',
+    'devServerPublic' => App::env('PRIMARY_SITE_URL') . ':3000',
+    'serverPublic' => App::env('PRIMARY_SITE_URL') . '/dist/',
+    'errorEntry' => '',
+    'cacheKeySuffix' => '',
+    'devServerInternal' => 'http://localhost:3000',
+    'checkDevServer' => true,
+    'includeReactRefreshShim' => false,
+    'includeModulePreloadShim' => true,
+    'criticalPath' => '@webroot/dist/criticalcss',
+    'criticalSuffix' =>'_critical.min.css',
+];
---
+++ web/dist/assets/main-Dh2Eezcx.js
@@ -0,0 +1,2 @@
+console.log("Hello World from index.ts");
+//# sourceMappingURL=main-Dh2Eezcx.js.map
